dbfef808f879285d61b8166731d998e51dec1a45
Samuel <samuelrivba@gmail.com>
2024-05-09 17:56:33 -0500

    TS variables

--- fundamentals.ts
+++ fundamentals.ts
@@ -1,1 +1,41 @@
-// Setting up typescript env
+// Variables
+
+const courseName = "Typescript";
+const courseName2 = "Typescript";
+
+const courseNam3 = `Typescript`;
+
+const amount = 10;
+const fraction = 10.5;
+
+const oneThousand = 1e3; // Exponents
+
+const allPermissions = 0o777; // Octals
+
+const hexByte = 0xff;
+
+const binary = 0b0100001;
+
+const bigInt = 9000n;
+
+const yes = true;
+const no = false;
+
+// Completely missing
+const missing = undefined;
+
+// We know it's missing
+const empty = null;
+
+let someNum = 0;
+someNum = 1;
+someNum = 2;
+
+// Variable shadowing
+{
+  let someNum = 5;
+}
+
+let hello; // Uninitialized
+
+// const whoops; Const must be initialized
