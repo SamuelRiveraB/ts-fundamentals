c765b2bbfb50b5b8b43ada670cbd40a24a25622c
Samuel <samuelrivba@gmail.com>
2024-06-02 19:33:04 -0500

    Add grep code

--- fundamentals.ts
+++ fundamentals.ts
@@ -341,3 +341,20 @@ export type Point = {
   x: number;
   y: number;
 };
+
+// Grep
+
+// import { readFileSync } from "fs";
+
+const args = process.argv.slice(2);
+const filename = args[0];
+const searchString = args[1];
+
+// const contents = readFileSync(filename, "utf8");
+// const lines = contents.split("\n");
+
+// for (let i = 0; i < ByteLengthQueuingStrategy.length; i++) {
+//   if (lines[i].includes(searchString)) {
+//     console.log(lines[i]);
+//   }
+// }
