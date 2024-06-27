8726273477889ad1029e851efeb4b0f158ddc217
Samuel <samuelrivba@gmail.com>
2024-06-26 19:25:04 -0500

    Add interfaces

--- intermediate.ts
+++ intermediate.ts
@@ -40,3 +40,48 @@ p.translate(-5, -5);
 
 const d = new Point(10, 10);
 d.translate(-5, -5);
+
+// Interfaces
+
+interface Area {
+  area(): number;
+}
+
+interface Perimeter {
+  perimeter(): number;
+}
+
+class Rectangle implements Area, Perimeter {
+  length: number = 1;
+  width: number = 1;
+
+  area(): number {
+    return this.length * this.width;
+  }
+
+  perimeter(): number {
+    return 2 * (this.length + this.width);
+  }
+}
+
+type AreaAndPerimeter = Area & Perimeter;
+
+class Circle implements AreaAndPerimeter {
+  radius: number = 4;
+
+  area(): number {
+    return Math.PI * this.radius ** 2;
+  }
+
+  perimeter(): number {
+    return 2 * Math.PI * this.radius;
+  }
+}
+
+const rect = new Rectangle();
+const circ = new Circle();
+
+const objectsWithArea: Area[] = [rect, circ];
+for (let i = 0; i < objectsWithArea.length; i++) {
+  console.log(objectsWithArea[i].area());
+}
