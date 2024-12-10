474cbfb23a6c3f834c0e84a4542d103d0c78d4f0
Samuel <samuelrivba@gmail.com>
2024-12-09 23:01:19 -0500

    Add iterators

--- advanced.ts
+++ advanced.ts
@@ -102,3 +102,44 @@ class Stack<T> {
 }
 
 const strings = new Stack<string>();
+
+// Iterators
+
+function* genValues(): Generator<number> {
+  yield 1;
+  yield 2;
+  yield 3;
+}
+
+const values = genValues();
+
+console.log(values.next());
+console.log(values.next());
+console.log(values.next());
+console.log(values.next());
+
+class RangeIter implements Iterable<number> {
+  private readonly start: number;
+  private readonly end: number;
+
+  constructor(start: number, end: number) {
+    this.start = start;
+    this.end = end;
+  }
+
+  *[Symbol.iterator](): Generator<number> {
+    for (let i = this.start; i <= this.end; i++) {
+      yield i;
+    }
+  }
+}
+
+const range = new RangeIter(1, 5);
+for (const num of range) {
+  console.log(num);
+}
+
+type CaseKinds = "lowercase" | "uppercase";
+function setCase(message: string, kind: "lowercase" | "uppercase"): string {
+  return "";
+}
